dd4dc84e3a053ccf0794477a68604481b7bcc2eb
lucrampro <lucienramassamy@gmail.com>
2018-07-03 15:12:14 +0200

    ajout de couleur verte sur le texte et d'une div ronde pour fair un effet scale en entrant sur la page

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
-TweenMax.to(".maskTxt", 1, { width:"550px", onComplete:function(){
+TweenMax.to(".round", 0.5, { css:{scale:0} , onComplete:function(){   
+
+    TweenMax.to(".maskTxt", 0.8, { width:"550px", onComplete:function(){
     
     
     TweenMax.to(".jaune", 0.5, { width:0});
     
-} }  );
+} }  )
+    
+
+} });
