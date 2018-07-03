f1c1f0226241eb906c8c49f6bbe99fdcee3f5195
lucrampro <lucienramassamy@gmail.com>
2018-07-03 16:00:48 +0200

    ajout d'une media querie et modif mineur sur le temps de l'animation

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-TweenMax.to(".round", 0.5, { css:{scale:0} , onComplete:function(){   
+TweenMax.to(".round", 0.8, { css:{scale:0} , onComplete:function(){   
 
     TweenMax.to(".maskTxt", 0.8, { width:"550px", onComplete:function(){
     
