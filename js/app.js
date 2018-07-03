2f7bdba8a5be77bca2b4417b659fd19fa4bb7f32
lucrampro <lucienramassamy@gmail.com>
2018-07-03 15:13:38 +0200

    l'animation de passage de la div jaune d'une width 100% a 0 se fait mintenan en 0.4s

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ TweenMax.to(".round", 0.5, { css:{scale:0} , onComplete:function(){
     TweenMax.to(".maskTxt", 0.8, { width:"550px", onComplete:function(){
     
     
-    TweenMax.to(".jaune", 0.5, { width:0});
+    TweenMax.to(".jaune", 0.4, { width:0});
     
 } }  )
     
