0ea5f77dec3ee0ca47c3b17e08a407e730d358d3
lucrampro <lucienramassamy@gmail.com>
2018-07-03 16:27:33 +0200

    ajout des trais de couleur et d'animation dessu PS: commencer en mobile first la prochaine fois..

--- js/app.js
+++ js/app.js
@@ -1,11 +1,39 @@
-TweenMax.to(".round", 0.8, { css:{scale:0} , onComplete:function(){   
 
-    TweenMax.to(".maskTxt", 0.8, { width:"550px", onComplete:function(){
+
+
+
+
+
+
+
+TweenMax.to(".round", 0.8, { css:{scale:0} , onComplete:function(){   
+TweenMax.to(".vert", 0.5, {width:"600px", onComplete: function(){
+    
+    TweenMax.to(".jaune", 0.5, {width:"500px", onComplete: function(){
+        
+        
+        
+        TweenMax.to(".pink", 0.5, {width:"500px", onComplete: function(){
+            
+            
+            
+            
+                TweenMax.to(".maskTxt", 0.8, { width:"550px", onComplete:function(){
     
     
-    TweenMax.to(".jaune", 0.4, { width:0});
+    TweenMax.to(".black", 0.4, { width:0});
     
 } }  )
+            
+            
+        }})
+        
+        
+        
+    }})
+    
+} }   )
+
     
 
 } });
