8514b52b94ccb6872af38a9d1979fd2121dc2f8d
lucrampro <lucienramassamy@gmail.com>
2018-07-04 11:13:03 +0200

    apres conversation avec Tristan toute l'animation a etais revu

--- js/app.js
+++ js/app.js
@@ -1,51 +1,45 @@
 
-
-TweenMax.to(".roundJ", 0.4, { css:{scale:0}, onComplete:function(){
+   
+TweenMax.to(".roundJ", 0.5, {css:{ scale:0 }});
+TweenMax.to(".roundV", 0.6, {css:{ scale:0 }, delay: 0.1});  
+TweenMax.to(".roundR", 0.7, {css:{ scale:0 }, delay: 0.2, onComplete:function(){
+    
+TweenMax.to(".vert", 0.5, {width:"600px"});
+TweenMax.to(".jaune", 0.5, {width:"500px", delay: 0.1});
+TweenMax.to(".pink", 0.5, {width:"500px", delay: 0.2, onComplete:function(){
     
     
-    TweenMax.to(".roundV", 0.4, { css:{scale:0}, onComplete:function(){
-        
-        
-       
-TweenMax.to(".roundR", 0.4, { css:{scale:0} , onComplete:function(){   
-TweenMax.to(".vert", 0.5, {width:"600px", onComplete: function(){
-    
-    TweenMax.to(".jaune", 0.5, {width:"500px", onComplete: function(){
-        
-        
-        
-        TweenMax.to(".pink", 0.5, {width:"500px", onComplete: function(){
-            
-            
-            
-            
-                TweenMax.to(".maskTxt", 0.8, { width:"550px", onComplete:function(){
+TweenMax.to(".maskTxt", 0.8, {height:"300px", onComplete:function(){
     
     
-    TweenMax.to(".black", 0.4, { width:0});
-    TweenMax.to(".GostTristan", 0.5, { width:"700px", delay:0.4});
+    
+      TweenMax.to(".GostTristan", 0.5, { width:"700px"});
     
     
-} }  )
-            
-            
-        }})
-        
-        
-        
-    }})
     
-} }   )
-
     
-
-} }) 
-        
-        
-    }})
+}});
+    
+    
     
 }});
 
 
-
-
+    
+    
+}});
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
